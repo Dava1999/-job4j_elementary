812370410e8e97547abc0f307fdbcd140abf8fe6
davidsabadas <davidka.sabadash@mail.ru>
2023-11-28 12:15:31 +0300

    Загрузка файлов на github [#243562]

--- src/main/java/ru/job4j/Main.java
+++ src/main/java/ru/job4j/Main.java
@@ -1,7 +1,10 @@
 package ru.job4j;
 
 public class Main {
+    private static String[] args;
+
     public static void main(String[] args) {
+        Main.args = args;
         System.out.println("Hello world");
     }
 }
