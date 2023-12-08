147aa1ca4e54d1b74d193555c3d3470e0e1a94c7
davidsabadas <davidka.sabadash@mail.ru>
2023-12-08 09:23:02 +0300

    Merge remote-tracking branch 'origin/master'

--- src/main/java/ru/job4j/Calculator.java
+++ src/main/java/ru/job4j/Calculator.java
@@ -1,17 +1,17 @@
 package ru.job4j.calculator;
 
-class Calculator {
+ public class Calculator {
 
     public static void main(String[] args) {
         int six = 6;
         int two = 2;
-        int sixDivTwo = six / two;
+        int sixDivTwo = (six / two);
         System.out.println(sixDivTwo);
         int five = 5;
-        int fiveMinusTwo = five - two;
+        int fiveMinusTwo = (five - two);
         System.out.println(fiveMinusTwo);
         int four = 4;
-        int fourTimeTwo = four * two;
+        int fourTimeTwo = (four * two);
         System.out.println(fourTimeTwo);
     }
 }
